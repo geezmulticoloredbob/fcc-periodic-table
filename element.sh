55496c68d444a684179ab595d60d43d9a3450ec9
Coen Walmsley <coenwalmsley@gmail.com>
2024-04-25 10:16:46 +0000

    feat: add atomic num arg

--- element.sh
+++ element.sh
@@ -8,3 +8,12 @@ then
   echo -e "\nPlease provide an element as an argument."
   exit
 fi
+
+#if argument is atomic number
+if [[ $1 =~ ^[1-9]+$ ]]
+then
+  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where atomic_number = '$1'")
+else
+#if argument is string
+  element=$($PSQL "select atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius from elements join properties using(atomic_number) join types using(type_id) where name = '$1' or symbol = '$1'")
+fi
