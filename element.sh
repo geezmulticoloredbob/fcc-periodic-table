958c3f9b2f28e928bff8a75fda0edc5a0837c30a
Coen Walmsley <coenwalmsley@gmail.com>
2024-04-25 10:16:04 +0000

    feat: add first if statement

--- element.sh
+++ element.sh
@@ -3,3 +3,8 @@
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --tuples-only -c"
 
 echo -e "\n\n~~~~ Periodic Table ~~~~\n\n"
+if [[ -z $1 ]]
+then
+  echo -e "\nPlease provide an element as an argument."
+  exit
+fi
